6b4ead0702c80e931f3f325503021a89ced23ae9
melhajjami <melhajjami8@boscdelacoma.cat>
2020-04-04 21:59:48 +0200

    Commit routing i 2 componenets
    login component i main page amb els testos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppRoutingModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
