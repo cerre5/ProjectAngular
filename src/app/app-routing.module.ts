8b5658061d4ec89305aaab643953e0625ca10838
melhajjami <melhajjami8@boscdelacoma.cat>
2020-04-04 22:03:30 +0200

    Routing funcionant

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'testos', component: MainPageComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
