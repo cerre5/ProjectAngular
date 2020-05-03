f21b7beec008bb4e64fdcfefc9a0292773309b7f
melhajjami <melhajjami8@boscdelacoma.cat>
2020-05-03 22:27:55 +0200

    2 Testos fets sense disseny

--- src/app/altre-test/altre-test.component.ts
+++ src/app/altre-test/altre-test.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AbstractControlDirective } from '@angular/forms';
+var divEsClickable = true;
+var nivell = 0;
+var numeroCorrecte = 0;
+var jocComensat = false;
+var mostrarTextArea;
 
 @Component({
   selector: 'app-altre-test',
@@ -8,12 +14,78 @@ import { Router } from '@angular/router';
 })
 export class AltreTestComponent implements OnInit {
 
+
   constructor(private router: Router) { }
 
   ngOnInit() {
+    mostrarTextArea = document.getElementById("comprovar");
+    mostrarTextArea.style.display = "none";
+  }
+
+  onDivClick() {
+    if (divEsClickable) {
+      this.ensenyarNumero();
+
+    }
+    /* 
+       nivell = nivell + 1;
+       ensenyar numero aleatori i posarlo en una variable
+       esperar uns seogons i cridar funcio que ensenyi el conjunt de textarea i que comprovo lo entrat amb la variable del AbstractControlDirective
+   */
+  }
+
+  generarAleatori(n) {
+    var add = 1, max = 12 - add;   // 12 is the min safe number Math.random() can generate without it starting to pad the end with zeros.   
+
+    if (n > max) {
+      return this.generarAleatori(max) + this.generarAleatori(n - max);
+    }
+
+    max = Math.pow(10, n + add);
+    var min = max / 10; // Math.pow(10, n) basically
+    var number = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    return ("" + number).substring(add);
+  }
+
+  ensenyarNumero() {
+    mostrarTextArea.style.display = "none";
+    jocComensat = true;
+    divEsClickable = false;
+    nivell = nivell + 1;
+    numeroCorrecte = this.generarAleatori(nivell);
+    document.getElementById("divClickable").innerHTML = "Nivell: " + nivell + "<br />" + numeroCorrecte.toString();
+
+    setTimeout(this.inputResposta, 3000);
+  }
+
+  inputResposta() {
+    mostrarTextArea.style.display = "block";
+    document.getElementById("divClickable").innerHTML = "Entra el numero que acabes de veure";
+  }
+
+  comprovarResposta(textareaValue) {
+    if (textareaValue != "") {
+      if (textareaValue == numeroCorrecte) {
+        this.ensenyarNumero();
+      } else {
+        this.gameover();
+      }
+    }
+  }
+
+  gameover() {
+    document.getElementById("divClickable").innerHTML = "Has completat " + (nivell-1) + " nivells, memoritzant fins a " + (nivell-1) + " xifres" + "<br />" + "clicka per tornar a jugar";
+    mostrarTextArea.style.display = "none";
+    jocComensat = false;
+    divEsClickable = true;
+    nivell = 0;
 
   }
+
   tornar() {
-    this.router.navigate(['main-page'])
+    if (!jocComensat) {
+      this.router.navigate(['main-page']);
+    }
   }
 }
--- src/app/reaction-speed/reaction-speed.component.ts
+++ src/app/reaction-speed/reaction-speed.component.ts
@@ -1,19 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+var jocComensat = false;
+var divEsClickable = true;
+var interval;
+var tempsAconseguit;
 
 @Component({
   selector: 'app-reaction-speed',
   templateUrl: './reaction-speed.component.html',
   styleUrls: ['./reaction-speed.component.css']
 })
-export class ReactionSpeedComponent implements OnInit {
 
+
+export class ReactionSpeedComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
+
   }
-  
+
   tornar() {
-    this.router.navigate(['main-page'])
+    if (!jocComensat) {
+      this.router.navigate(['main-page']);
+    }
+  }
+
+  onDivClick() {
+    if (!jocComensat && divEsClickable) {
+      jocComensat = true;
+      document.getElementById("divclickable").style.backgroundColor = "yellow";
+      document.getElementById("divclickable").innerHTML = "espera al color verd i clica";
+      var numeroAleatori = Math.floor(Math.random() * (+5000 - +2000)) + +2000;
+      this.esperarTempsAleatori(numeroAleatori);
+    } else if(jocComensat && divEsClickable) {
+      clearInterval(interval);
+      //AQUI ESTA EL VALOR PER PUJAR A LA BASE DE DADES SI ES EL SEU MILLOR TEMPS
+      console.log(tempsAconseguit);
+      document.getElementById("divclickable").innerHTML = document.getElementById("divclickable").innerHTML + "<br />" + "torna a clickar  ";
+      jocComensat = false;
+    }
+  }
+
+  esperarTempsAleatori(numeroAleatori) {
+    divEsClickable = false;
+    setTimeout(this.comensarJoc, numeroAleatori);
+  }
+  comensarJoc() {
+    divEsClickable=true;
+    var startTime = Date.now();
+    document.getElementById("divclickable").style.backgroundColor = "green";
+    interval = setInterval(function () {
+      var elapsedTime = Date.now() - startTime;
+      document.getElementById("divclickable").innerHTML = (elapsedTime / 1000).toFixed(3);
+      tempsAconseguit = (elapsedTime / 1000).toFixed(3);
+    }, 100);
   }
 }
